34c90af872e14abac5f948c7f8334d04b77c1117
SaiRohanReddy <61152611+SaiRohanReddy@users.noreply.github.com>
2020-09-30 22:07:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var player1 = prompt("Player 1 Name: ");
+var player2 = prompt("Player 2 Name: ");
 function playFunction(){
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomImage = "dice" + randomNumber1;
@@ -8,12 +10,12 @@ document.querySelector(".img2").setAttribute("src","images/" + randomImage1 + ".
 
 if (randomNumber1 > randomNumber2) {
 
-    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins";
+    document.querySelector("h1").innerHTML = "🚩" +  Player1 + " Wins";
 }
 
 if (randomNumber2 > randomNumber1) {
 
-    document.querySelector("h1").innerHTML = "🚩 Player 2 Wins";
+    document.querySelector("h1").innerHTML = "🚩" + Player2 +" Wins";
 }
 
 if (randomNumber1 === randomNumber2) {
